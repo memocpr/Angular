a7e713b6c26ac638e6bf02a2d6657a9366606bbf
Copur Mehmet, INI-DNA-NB <Mehmet.Copur@swisscom.com>
2021-09-19 10:18:29 +0200

    task 2 is done

--- angularTasks/basics-ngfor-addition/src/app/app.module.ts
+++ angularTasks/basics-ngfor-addition/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
