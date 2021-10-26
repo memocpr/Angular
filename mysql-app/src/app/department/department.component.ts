8a1cd63e4ade18a48e451f1b37dc51d434a84b34
Copur Mehmet, INI-DNA-NB <Mehmet.Copur@swisscom.com>
2021-10-26 15:49:55 +0200

    buttons are working

--- mysql-app/src/app/department/department.component.ts
+++ mysql-app/src/app/department/department.component.ts
@@ -12,7 +12,11 @@ export class DepartmentComponent implements OnInit {
   constructor(private http:HttpClient) { }
 
   departments:any=[];
-  departmentEndPoint='department'
+  departmentEndPoint='department/';
+
+  modalTitle="";
+  DepartmentId=0;
+  DepartmentName="";
 
   ngOnInit(): void {
     this.refreshList();
@@ -24,6 +28,51 @@ export class DepartmentComponent implements OnInit {
       this.departments=data;
       console.log("Departments : " +this.departments)
     });
+  }
+
+  onAdd(){
+    this.modalTitle="Add Department";
+    this.DepartmentId=0;
+    this.DepartmentName="";
+  }
+
+  onEdit(oneDep:any){
+    this.modalTitle="Edit Department";
+    this.DepartmentId=oneDep.DepartmentId;
+    this.DepartmentName=oneDep.DepartmentName;
+  }
+
+  onCreate(){
+    var value={
+      DepartmentName:this.DepartmentName
+    };
+
+    this.http.post(environment.API_URL+this.departmentEndPoint,value).subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  onUpdate(){
+    var value={
+      DepartmentId:this.DepartmentId,
+      DepartmentName:this.DepartmentName
+    };
+
+    this.http.put(environment.API_URL+this.departmentEndPoint,value).subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  onDelete(id:any){
+
+    if(confirm('Are You Sure???')){
+      this.http.delete(environment.API_URL+this.departmentEndPoint+id).subscribe(res=>{
+        alert(res.toString());
+        this.refreshList();
+      });
+    }
   }
 
 }
